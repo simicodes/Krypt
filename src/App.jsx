e39d45bfc80b2c0d46cf79d5cc51a9cadb334eec
simicodes <simeonoffiong@gmail.com>
2022-09-07 16:08:16 +0100

    Web 3.0 Project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  Navbar,
+  Footer,
+  Welcome,
+  Services,
+  Transactions,
+} from "./components";
+const App = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
