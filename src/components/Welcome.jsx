80da368b07056814409f8673fb1fec4f9c4c7db3
simicodes <simeonoffiong@gmail.com>
2022-09-10 15:39:47 +0100

    Updated...

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -60,7 +60,7 @@ const Welcome = () => {
               className="flex flex-row justify-center items-center 
           my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd] "
             >
-              <AiFillPlayCircle />
+              {/* <AiFillPlayCircle /> */}
               <p className="text-white text-base font-semibold">
                 Connect Wallet
               </p>
